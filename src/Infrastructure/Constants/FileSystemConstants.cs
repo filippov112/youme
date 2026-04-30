c7e3e3bc26c6bef3e5081e56b869fea8ccb376c3
Filippov Ilya <ilya19xx2014@gmail.com>
2026-04-30 23:19:41 +0300

    Добавил константы файловой системы.

---
+++ src/Infrastructure/Constants/FileSystemConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Constants
+{
+    internal class FileSystemConstants
+    {
+        public const string GlobalConfigPath = "./config/config.json";
+        public const string LocalConfigFileName = "config.json";
+        public const string LocalConfigFolder = ".schiza";
+    }
+}
