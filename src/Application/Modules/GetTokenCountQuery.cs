a6719b10460da288e8fa5602e2db313f8f3d9985
Filippov Ilya <ilya19xx2014@gmail.com>
2026-05-02 12:38:14 +0300

    Добавил запрос на подсчет кол-ва токенов.

---
+++ src/Application/Modules/GetTokenCountQuery.cs
@@ -0,0 +1,14 @@
+﻿using Application.Interfaces;
+using MediatR;
+
+namespace Application.Modules
+{
+    public record GetTokenCountQuery(string prompt) : IRequest<int>;
+    public class GetTokenCountQueryHandler(ITokenCounter counter) : IRequestHandler<GetTokenCountQuery, int>
+    {
+        public async Task<int> Handle(GetTokenCountQuery request, CancellationToken cancellationToken)
+        {
+            return counter.CalcTokenCount(request.prompt);
+        }
+    }
+}
